ae43a06f46fff4b6edd1029de7daaddc12d27953
Valley-3333 <ahousute-ahou@yahoo.co.jp>
2024-04-02 13:20:01 +0900

    EU_import

---
+++ common/defines/01_defines.lua
@@ -0,0 +1,5 @@
+NDefines.NFocus.FOCUS_PROGRESS_PEACE = 1;					-- Progress during peace
+NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.15;			-- Each province owned by the target country contributes this amount of volunteers to the limit.
+NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.10;				-- Each army unit owned by the source country contributes this amount of volunteers to the limit.
+NDefines.NDiplomacy.VOLUNTEERS_RETURN_EQUIPMENT = 1.0;				-- Returning volunteers keep this much equipment
+NDefines.NDiplomacy.VOLUNTEERS_TRANSFER_SPEED = 0;					-- days to transfer a unit to another nation
